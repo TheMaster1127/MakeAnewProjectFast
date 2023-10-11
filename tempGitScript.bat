655cb10a6873872c6d6c99c2fd3df2be3b131fd0
TheMaster1127 <Dzemilefere@gmail.com>
2023-10-11 23:58:15 +0300

    Added a LICENCE

--- tempGitScript.bat
+++ tempGitScript.bat
@@ -1,11 +1,7 @@
 cd C:\Users\The_M\OneDrive\Desktop\GitHub Projects\THE_AI_COMBINE_2.0_IN_AHK\THE_AI_COMBINE_2.0_IN_AHK
-git init
-
-git branch -m main
 
 git add .
 
-git commit -m "Create README.md"
-
+git commit -m "Added a LICENCE"
 
 git push origin main
