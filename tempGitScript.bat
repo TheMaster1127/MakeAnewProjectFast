5ba54956fc3b6f382c0616c3168bbb0c6a8c3984
TheMaster1127 <email@example.com>
2023-10-13 12:23:55 +0300

    Added a LICENCE

--- tempGitScript.bat
+++ tempGitScript.bat
@@ -1,11 +1,7 @@
 cd C:\Users\The_M\OneDrive\Desktop\GitHub Projects\Testing12345\Testing12345
-git init
-
-git branch -m main
 
 git add .
 
-git commit -m "Create README.md"
-
+git commit -m "Added a LICENCE"
 
 git push origin main
