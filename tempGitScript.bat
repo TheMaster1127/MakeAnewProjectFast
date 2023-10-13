a96788e4b52120eba841a3c13721383776e751ff
TheMaster1127 <Dzemilefere@gmail.com>
2023-10-13 12:26:26 +0300

    Create README.md

--- tempGitScript.bat
+++ tempGitScript.bat
@@ -1,7 +1,11 @@
-cd C:\Users\The_M\OneDrive\Desktop\GitHub Projects\Testing12345\Testing12345
+cd C:\Users\The_M\OneDrive\Desktop\GitHub Projects\Testing123456\Testing123456
+git init
+
+git branch -m main
 
 git add .
 
-git commit -m "Added a LICENCE"
+git commit -m "Create README.md"
+
 
 git push origin main
